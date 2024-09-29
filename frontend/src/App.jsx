1fb31bb17dd14f5fbe618544d5fde40cd4dfc215
FrancoDominguez <franco@wearecws.com>
2024-09-28 22:55:31 -0400

    creating basic react app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="">LANDING PAGE</div>
+    </div>
+  );
+}
+
+export default App;
