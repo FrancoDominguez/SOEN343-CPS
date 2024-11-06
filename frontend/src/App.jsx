fc53be6ceb62b00aa22a8ed7efc6847cbbb92547
FrancoDominguez <franco@wearecws.com>
2024-11-05 22:34:10 -0500

    added the orderform page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,36 @@
 import { useContext, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ApiContext } from "./components/ApiProvider";
+import OrderForm from "./components/OrderForm";
 
 function App() {
   const GoogleApiKey = useContext(ApiContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     console.log("api key", GoogleApiKey);
   }, [GoogleApiKey]);
 
+  function handleButtonClick() {
+    navigate("/order");
+  }
+
   return (
-    <Router>
-      <div className="flex justify-center items-center min-h-screen">
-        <Routes>
-          <Route path="/" element={<div>LANDING PAGE</div>} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="flex justify-center items-center">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <div>Landing page</div>
+              <button onClick={handleButtonClick}>clickme!</button>
+            </div>
+          }
+        />
+        <Route path="/order" element={<OrderForm />} />
+      </Routes>
+    </div>
   );
 }
 
--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ApiProvider } from "./components/ApiProvider";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
-  <ApiProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </ApiProvider>
+  <Router>
+    <ApiProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </ApiProvider>
+  </Router>
 );
