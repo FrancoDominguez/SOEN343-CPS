26f792fa0f8cc8e184176900b935313ce2e84b1a
Antoine Mansour <129776972+antoinemansour7@users.noreply.github.com>
2024-11-12 23:37:45 -0500

    Added signup feature

--- backend/apiTest.py
+++ backend/apiTest.py
@@ -1,12 +1,19 @@
 import requests
 
-url = "http://localhost:8080/login"
+url = "http://localhost:8080/signup"
 data = {
-    "email": "hoboslime@gmail.com",
+    "firstname": "antoine",
+    "lastname": "mansour",
+    "email": "antoine.rmcr77@gmail.com",
     "password": "supersafe"
 }
 
 response = requests.post(url, json=data)
-print(response.json())
 
-
+try:
+    # Try to parse JSON if available
+    response_json = response.json()
+    print("Response JSON:", response_json)
+except ValueError:
+    # Handle the case where the response is plain text
+    print("Response is plain text:", response.text)
--- backend/src/main/java/cps/models/User.java
+++ backend/src/main/java/cps/models/User.java
@@ -1,58 +1,77 @@
 package cps.models;
 
+import cps.services.Mysqlcon;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class User {
-  private int id;
-  private String firstname;
-  private String lastname;
-  private String email;
-  private String password;
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
 
-  public int getId() {
-    return this.id;
-  }
-
-  public String getFirstname() {
-    return this.firstname;
-  }
-
-  public String getLastname() {
-    return this.lastname;
-  }
-
-  public String getEmail() {
-    return this.email;
-  }
-
-  // new user constructor
-  public User(String firstname, String lastname, String email, String password) {
-    this.id = -1;
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.email = email;
-    this.password = password;
-  }
-
-  // user retrieved from database
-  public User(int id, String firstname, String lastname, String email, String password) {
-    this.id = id;
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.email = email;
-    this.password = password;
-  }
-
-  public boolean validatePassword(String password) {
-    return this.password.equals(password);
-  }
-
-  public void save() {
-    if (this.id == -1) {
-      String queryString = "";
-      // create new user
-    } else {
-      String queryString = "";
-      // update values to mysql
-
+    public int getId() {
+        return this.id;
     }
-  }
+
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    public String getLastname() {
+        return this.lastname;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    // Constructor for a new user
+    public User(String firstname, String lastname, String email, String password) {
+        this.id = -1;  // -1 indicates a new user
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Constructor for an existing user (retrieved from the database)
+    public User(int id, String firstname, String lastname, String email, String password) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public boolean validatePassword(String password) {
+        return this.password.equals(password);
+    }
+
+    // Save or update user in the database
+    public void save() {
+        try {
+            Mysqlcon mysqlcon = new Mysqlcon();
+            mysqlcon.connect();
+            if (this.id == -1) {
+                // Insert new user
+                String insertSQL = String.format(
+                    "INSERT INTO clients (firstname, lastname, email, password) VALUES ('%s', '%s', '%s', '%s')",
+                    this.firstname, this.lastname, this.email, this.password);
+                String result = mysqlcon.executeUpdate(insertSQL);
+
+                
+            } else {
+                // Update existing user
+                String updateSQL = String.format(
+                    "UPDATE clients SET firstname='%s', lastname='%s', email='%s', password='%s' WHERE id=%d",
+                    this.firstname, this.lastname, this.email, this.password, this.id);
+                String result = mysqlcon.executeUpdate(updateSQL);
+                System.out.println(result);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
 }
