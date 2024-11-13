f0bf4b31fbe23461bcc9a5e6782304722b91d807
Hudson Lu <131294600+HudsonLu@users.noreply.github.com>
2024-11-13 12:33:27 -0500

    Payment front end

--- backend/src/main/java/cps/services/StripeConfig.java
+++ backend/src/main/java/cps/services/StripeConfig.java
@@ -2,11 +2,13 @@
 package cps.services;
 import org.springframework.context.annotation.Configuration;
 import com.stripe.Stripe;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @Configuration
 public class StripeConfig {
     public StripeConfig() {
         // Set your Stripe Secret Key
-        Stripe.apiKey = "sk_test_51QJSNLDKF4U2gF7ZGUkw01LQnan2cRPlhJDAtFIog2GpK9G5LoAdFVKR94k9AXJiuFewPO70nJbb7XWxaV4fSrgG00djB7W7Pp";
+        Dotenv dotenv = Dotenv.load();
+        Stripe.apiKey = dotenv.get("STRIPE_SECRET_KEY");
     }
 }
