6d2135e9f3ad468a713c493e2cc83cc699d5d2ae
Ashkan Forghani <ashkanf@hotmail.com>
2024-11-28 16:37:29 -0500

    fix

---
+++ backend/src/main/java/cps/DTO/RequestBodies/CreateDelivReqBody.java
@@ -0,0 +1,14 @@
+package cps.DTO.RequestBodies;
+
+public class CreateDelivReqBody {
+  int contractId;
+
+  public CreateDelivReqBody() {
+
+  }
+
+  public int getContractId() {
+    return this.contractId;
+  }
+
+}
